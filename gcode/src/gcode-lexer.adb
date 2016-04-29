3731fa07c5147834edd8caa836dad02e63cf9bfd
Fabien Chouteau <chouteau@adacore.com>
2016-04-29 13:10:07 +0200

    Copyright and licensing

--- gcode/src/gcode-lexer.adb
+++ gcode/src/gcode-lexer.adb
@@ -1,3 +1,25 @@
+-------------------------------------------------------------------------------
+--                                                                           --
+--                                   ACNC                                    --
+--                                                                           --
+--         Copyright (C) 2016 Fabien Chouteau (chouteau@adacore.com)         --
+--                                                                           --
+--                                                                           --
+--    ACNC is free software: you can redistribute it and/or modify it        --
+--    under the terms of the GNU General Public License as published by      --
+--    the Free Software Foundation, either version 3 of the License, or      --
+--    (at your option) any later version.                                    --
+--                                                                           --
+--    ACNC is distributed in the hope that it will be useful, but WITHOUT    --
+--    ANY WARRANTY; without even the implied warranty of MERCHANTABILITY     --
+--    or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public        --
+--    License for more details.                                              --
+--                                                                           --
+--    You should have received a copy of the GNU General Public License      --
+--    along with ACNC. If not, see <http://www.gnu.org/licenses/>.           --
+--                                                                           --
+-------------------------------------------------------------------------------
+
 with Ada.Text_IO; use Ada.Text_IO;
 with Gcode.Error; use Gcode.Error;
 
